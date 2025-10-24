3ffccbdaaaa7bfe71aabfa76a40185822c970379
Osman Tufekci <osmantufekci41@gmail.com>
2025-10-24 15:36:59 +0300

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "ArcRaidersData",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "ArcRaidersData",
+            targets: ["ArcRaidersData"]),
+    ],
+    targets: [
+        .target(
+            name: "ArcRaidersData",
+            dependencies: [],
+            resources: [
+                .process("Data")
+            ]
+        )
+    ]
+)
