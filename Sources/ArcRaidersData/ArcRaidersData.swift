8f2f247c606e78bf7488b16d60ba59dc767b83a5
Osman T <admin@osmantufekci.com>
2025-10-24 17:55:15 +0300

    added module property

--- Sources/ArcRaidersData/ArcRaidersData.swift
+++ Sources/ArcRaidersData/ArcRaidersData.swift
@@ -3,3 +3,24 @@ import Foundation
 public class ArcRaidersDataInfo {
     public static let version = "1.0.0"
 }
+
+extension Foundation.Bundle {
+    /// Returns the resource bundle associated with the current Swift module.
+    static var module: Bundle = {
+        let bundleName = "ArcRaidersData_ArcRaidersData"
+        
+        let candidates = [
+            Bundle.main.resourceURL,
+            Bundle(for: ArcRaidersDataInfo.self).resourceURL,
+            Bundle.main.bundleURL,
+        ]
+        
+        for candidate in candidates {
+            let bundlePath = candidate?.appendingPathComponent(bundleName + ".bundle")
+            if let bundle = bundlePath.flatMap(Bundle.init(url:)) {
+                return bundle
+            }
+        }
+        fatalError("unable to find bundle named BioSwift_BioSwift")
+    }()
+}
